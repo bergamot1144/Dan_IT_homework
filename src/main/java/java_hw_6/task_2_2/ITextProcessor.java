fabdc089524e259d0bc03d3a47cae901c65dfd5f
DMYTRO POLONSKYI <bergamot.ads1@gmail.com>
2025-08-07 01:30:47 +0300

    fixed hw 6

--- src/main/java/java_hw_6/task_2_2/ITextProcessor.java
+++ src/main/java/java_hw_6/task_2_2/ITextProcessor.java
@@ -1,9 +1,9 @@
 package java_hw_6.task_2_2;
 
 interface ITextProcessor {
-    public String process(String text);
+    String process(String text);
 
-    public default String removeDigits(String text) {
+    default String removeDigits(String text) {
         return text.replace(".", "");
     }
 }
