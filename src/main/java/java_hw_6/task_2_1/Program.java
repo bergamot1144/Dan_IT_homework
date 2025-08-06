a7933f65f28e17c9feb9d873555a66aefb81a6e6
DMYTRO POLONSKYI <bergamot.ads1@gmail.com>
2025-08-07 01:35:27 +0300

    fixed hw 6

--- src/main/java/java_hw_6/task_2_1/Program.java
+++ src/main/java/java_hw_6/task_2_1/Program.java
@@ -9,18 +9,19 @@ public class Program {
         System.out.println("Area of the circle " + circleArea);
         double circlePerimeter = circle.getPerimeter();
         System.out.println("Perimeter of the circle " + circlePerimeter);
+
         Figure square = new Square(5.0);
+
         double squareArea = square.getArea();
         System.out.println("Area of the square " + squareArea);
         double squarePerimeter = square.getPerimeter();
         System.out.println("Perimeter of the square " + squarePerimeter);
 
         Figure triangle = new Triangle(3.0, 4.0, 5.0);
+
         double triangleArea = triangle.getArea();
         System.out.println("Area of the triangle " + triangleArea);
         double trianglePerimeter = triangle.getPerimeter();
         System.out.println("Perimeter of the triangle " + trianglePerimeter);
-
     }
-
 }
